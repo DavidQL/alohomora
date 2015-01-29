652308ca9a27f33c8219836c7b1f328a5b7ac19c
David Aragon <david@quickleft.com>
2015-01-28 20:48:39 -0700

    commenting

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     $('.achievement').text("");
   });
 
+  alert("How to perform spells:\nLumos: forward flick\nExpelliarmus: selfie\nWingardium Leviosa: table\nAlohomora: down angled forward flick")
+
   window.ondevicemotion = function(event) {
     var x = Math.floor(event.accelerationIncludingGravity.x);
     var y = Math.floor(event.accelerationIncludingGravity.y);
@@ -11,17 +13,17 @@ $(document).ready(function() {
 
     $('.data').text('x: ' + x + ' y: ' + y);
 
-    if (x === -10 && y === -1) {
+    if (x === -10 && y === -1) { // forward flick
       spell = "Lumos!"
       $('body').attr('class', 'one')
-    } else if (x === 0 && y === -10) {
+    } else if (x === 0 && y === -10) { // selfie
       spell = "Expelliarmus!"
       $('body').attr('class', 'two')
-    } else if (x === 0 && y === 0) {
+    } else if (x === 0 && y === 0) { // table
       spell = "Wingardium Leviosa!"
       $('body').attr('class', 'three')
-    } else if (x === -6 && y === 0) {
-      spell = "Accio"
+    } else if (x === -6 && y === 0) { // down angled forward flick
+      spell = "Alohomora"
       $('body').attr('class', 'four')
     }
     $('.achievement').text(spell);
